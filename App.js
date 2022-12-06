66c751730a08a60f18d9621f16dfb0c65e66550e
Keannu Bernasol <noehbernasol0@gmail.com>
2022-12-06 12:14:53 +0800

    Added styling to windows and background. Also added initial NavigationContainer

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import styles from './styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.background}>
+        <View style={styles.window}>
+          <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        </View>
+      </View>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ styles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        backgroundColor: '#33464d',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+    },
+    window: {
+        alignContent: 'center',
+        height: '30%',
+        width: '50%',
+        backgroundColor: '#151a19',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default styles;
