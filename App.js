28ff884817c1af91d76ee659fce85f23da6ff264
Keannu Bernasol <noehbernasol0@gmail.com>
2022-12-06 23:09:31 +0800

    Tidied up code and separated Drawer content and settings onto separate .js file

--- App.js
+++ App.js
@@ -1,27 +1,17 @@
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { View, Image, TouchableOpacity } from 'react-native';
-import {
-  createDrawerNavigator,
-  DrawerContentScrollView,
-  DrawerItemList,
-  DrawerItem,
-} from '@react-navigation/drawer';
-import styles from './styles';
 
+// Navigation imports
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// Drawer settings & Drawer content/styling imports
+import DrawerScreenSettings from './Components/Drawer/DrawerScreenSettings';
+import CustomDrawerContent from './Components/Drawer/CustomDrawerContent';
+
+//Pages imports
 import Homepage from './Pages/Homepage/Homepage';
 
-function CustomDrawerContent(props) {
-  return (
-    <DrawerContentScrollView {...props}>
-      <DrawerItemList {...props} />
-      <DrawerItem
-        label="Heh"
-        onPress={() => props.navigation.toggleDrawer()}
-      />
-    </DrawerContentScrollView>
-  );
-}
+
 
 const Drawer = createDrawerNavigator();
 
@@ -30,16 +20,7 @@ function MyDrawer({ navigation }) {
     <Drawer.Navigator
       drawerContent={(props) => <CustomDrawerContent {...props} />}
       initialRouteName='Home'
-      screenOptions={{
-        headerTitleStyle: { color: 'white', fontWeight: 'bold', fontSize: 26, },
-        headerStyle: { backgroundColor: '#151a19', },
-        headerTintColor: 'white',
-        headerRight: () => (
-          <View style={{ flexDirection: 'row' }}>
-            <Image style={styles.navbar_image} source={require('./img/App-logo.png')} />
-          </View>
-        )
-      }}>
+      screenOptions={DrawerScreenSettings}>
       <Drawer.Screen name="Home" component={Homepage} />
     </Drawer.Navigator>
   );
